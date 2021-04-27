644faf880308bb11868f7df1b1cdb17f1d629edb
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-04-27 13:37:03 +0530

    adding logging

--- manage.py
+++ manage.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from waitress import serve
 
-from src.main import app, db
+
+from src.app import app, db
+from src.config import logging
 
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    print("Starting server...")
+    logging.info("Starting server...")
     serve(app, host="0.0.0.0", port=8000)
--- src/config.py
+++ src/config.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-
+import logging
 load_dotenv()
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)8s --- %(message)s ' +'(%(filename)s:%(lineno)s)',
+					datefmt='%d/%m/%Y %I:%M:%S %p',
+					level=logging.INFO,
+					handlers=[
+        				logging.FileHandler("debug.log", mode='w'),
+        				logging.StreamHandler()])
 
 def create_app():
     app = Flask(__name__)
